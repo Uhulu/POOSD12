614de8b8883afabe25e87179bdd0d763ad3c9e5e
Armando Diaz <adiaz32735@gmail.com>
2024-09-12 20:50:21 -0400

    Add files via upload

--- mysqldump/mysqldump.sql
+++ mysqldump/mysqldump.sql
@@ -1,1 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+--
+-- Host: localhost    Database: COP4331
+-- ------------------------------------------------------
+-- Server version	8.0.39-0ubuntu0.22.04.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Colors`
+--
+
+DROP TABLE IF EXISTS `Colors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Colors` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL DEFAULT '',
+  `UserID` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Colors`
+--
+
+LOCK TABLES `Colors` WRITE;
+/*!40000 ALTER TABLE `Colors` DISABLE KEYS */;
+INSERT INTO `Colors` VALUES (4,'red',1),(9,'blue',4),(11,'Purple',1),(12,'Pink',1),(13,'Blue',5),(14,'blue',9),(15,'Green',1),(16,'Indigo',1),(17,'red',5),(18,'orange',5),(19,'magenta',10),(20,'magenta',10),(21,'Neon Green',10),(22,'Neon Green',10),(23,'Blue',1),(24,'Brown',1);
+/*!40000 ALTER TABLE `Colors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Contacts`
+--
+
+DROP TABLE IF EXISTS `Contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Contacts` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL DEFAULT '',
+  `LastName` varchar(50) NOT NULL DEFAULT '',
+  `PhoneNumber` varchar(50) NOT NULL DEFAULT '',
+  `EmailAddress` varchar(50) NOT NULL DEFAULT '',
+  `UserID` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Contacts`
+--
+
+LOCK TABLES `Contacts` WRITE;
+/*!40000 ALTER TABLE `Contacts` DISABLE KEYS */;
+INSERT INTO `Contacts` VALUES (2,'Stinky','Rooster','123-456-7890','john.doe@example.com',69),(3,'Gloria','Sparks','407-923-5432','gloriarocks@example.com',249),(4,'Albert','Einstein','234-763-3454','spaceship156@example.com',249),(5,'John','Doe','123-456-7890','example@example.com',71),(8,'Beth','Davis','904-128-3742','sdavis222@example.com',21),(10,'Daisy','May','904-128-3742','sdavis222@example.com',65);
+/*!40000 ALTER TABLE `Contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `DateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `DateLastLoggedIn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `FirstName` varchar(50) NOT NULL DEFAULT '',
+  `LastName` varchar(50) NOT NULL DEFAULT '',
+  `Login` varchar(50) NOT NULL DEFAULT '',
+  `Password` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'2024-08-24 15:29:58','2024-08-24 15:29:58','Rick','Leinecker','RickL','COP4331'),(2,'2024-08-24 15:30:15','2024-08-24 15:30:15','Sam','Hill','SamH','Test'),(5,'2024-08-24 16:06:31','2024-08-24 16:06:31','Poosd','Group','poosd12','taco'),(6,'2024-08-28 15:28:42','2024-08-28 15:28:42','David','Wilson','davidw','penelope71'),(7,'2024-08-28 15:32:33','2024-08-28 15:32:33','Noah','Scott','noahb20','no@h28'),(8,'2024-08-28 15:33:36','2024-08-28 15:33:36','Mia','Anderson','mamamia','anderson832'),(9,'2024-08-31 01:58:00','2024-08-31 01:58:00','Arm','Diaz','adiaz','test'),(10,'2024-09-02 00:47:41','2024-09-02 00:47:41','Julian','Mendis','JulianM','COP4331'),(11,'2024-09-05 22:06:58','2024-09-05 22:06:58','Stephaine','Mendez','steph123','mendez453');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-12 22:50:05
